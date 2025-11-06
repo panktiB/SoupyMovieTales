47914ae1cab0d4b895ff9ec0f4cafab7e0dceeb7
panktiB <mishthii95@hotmail.com>
2025-11-06 12:07:53 +0530

    update: tested tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="min-h-screen bg-gray-900 text-white p-8">
+      <h1 className="text-4xl font-bold">IMDb React App</h1>
+      <p className="text-gray-400 mt-2 bg-purple">Tailwind is working!</p>
+      <div className="bg-purple-600">testing</div>
+    </div>
   )
 }
 
